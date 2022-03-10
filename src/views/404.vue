71ba2eb7eab327f4d144db7bbdbe0d3c2d9bf9a2
maudetes <maudet.estelle@gmail.com>
2022-03-10 17:56:23 +0100

    Frenchize the 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="fr-p-md-6w">
-    <h1>(404) Page NotFound</h1>  </div>
+    <h1>(404) Page non trouvée</h1>
+    <h2>La page que vous cherchez ne peut être trouvée.</h2>
+  </div>
 </template>
 
 <script>
